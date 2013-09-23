c744938e9ca598d76256ee4d87ea543a73a9966e
ht92 <ht92@live.ca>
2013-09-22 22:33:30 -0700

    Started Exercise 3
    Wrote the code. But not tested, probably doesn't work yet.

---
+++ Exercise3/exercise_three.vhd
@@ -0,0 +1,110 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all; 
+USE ieee.std_logic_arith.all; 
+USE ieee.std_logic_unsigned.all; 
+
+entity exercise_three is
+	port (
+		SW : IN std_logic_vector(0 downto 0);
+      KEY : IN std_logic_vector(3 DOWNTO 0);
+      CLOCK_50 : IN std_logic;
+      DRAM_CLK, DRAM_CKE : OUT std_logic;
+      DRAM_ADDR : OUT std_logic_vector(11 DOWNTO 0);
+      DRAM_BA_0, DRAM_BA_1 : BUFFER std_logic;
+      DRAM_CS_N, DRAM_CAS_N, DRAM_RAS_N, DRAM_WE_N : OUT std_logic;
+      DRAM_DQ : INOUT std_logic_vector(15 DOWNTO 0);
+      DRAM_UDQM, DRAM_LDQM : BUFFER std_logic;
+	
+		LCD_DATA : INOUT std_logic_vector(7 DOWNTO 0);
+		LCD_ON : OUT std_logic;
+		LCD_BLON : OUT std_logic;
+		LCD_EN : OUT std_logic;
+		LCD_RS : OUT std_logic;
+		LCD_RW : OUT std_logic;
+		
+		SD_CMD : inout std_logic;
+		SD_DAT : inout std_logic;
+		SD_DAT3 : inout std_logic;
+		SD_CLK : out std_logic
+	 
+	);
+end exercise_three;
+
+architecture exercise_three_structure of exercise_three is
+	component nios_system_SDRAM_LCD_allTimers_SDCARD port (
+			clk_clk : in std_logic;
+			reset_reset_n : in std_logic;
+			keys_export : in std_logic_vector(3 downto 0);
+			
+			sdram_clk_clk : out std_logic;
+			sdram_wire_addr : out std_logic_vector(11 downto 0);
+			sdram_wire_ba : buffer std_logic_vector(1 downto 0);
+			sdram_wire_cas_n : out std_logic;
+			sdram_wire_cke : out std_logic;
+			sdram_wire_cs_n : out std_logic;
+			sdram_wire_dq : inout std_logic_vector(15 downto 0);
+			sdram_wire_dqm : buffer std_logic_vector(1 downto 0);
+			sdram_wire_ras_n : out std_logic;
+			sdram_wire_we_n : out std_logic;
+			
+			lcd_data_DATA : inout std_logic_vector(7 downto 0);
+			lcd_data_ON : out std_logic;
+			lcd_data_BLON : out std_logic;
+			lcd_data_EN : out std_logic;
+			lcd_data_RS : out std_logic;
+			lcd_data_RW : out std_logic;	
+			
+			sd_card_interface_b_SD_cmd : inout std_logic;
+			sd_card_interface_b_SD_dat : inout std_logic;
+			sd_card_interface_b_SD_dat3 : inout std_logic;
+			sd_card_interface_o_SD_clock : out std_logic);
+	end component;
+	
+
+
+   signal DQM : std_logic_vector(1 DOWNTO 0);
+   signal BA : std_logic_vector(1 DOWNTO 0);
+	
+begin
+	DRAM_BA_0 <= BA(0);
+	DRAM_BA_1 <= BA(1);
+	DRAM_UDQM <= DQM(1);
+	DRAM_LDQM <= DQM(0);
+	
+	NiosII : nios_system_SDRAM_LCD_allTimers_SDCARD port map (
+	
+		 clk_clk => CLOCK_50,
+		 reset_reset_n => not(SW(0)),   --change
+		 keys_export => KEY(3 downto 0),
+		 
+		 sdram_clk_clk => DRAM_CLK,
+		 sdram_wire_addr => DRAM_ADDR,
+		 sdram_wire_ba => BA,
+		 sdram_wire_cas_n => DRAM_CAS_N,
+		 sdram_wire_cke => DRAM_CKE,
+		 sdram_wire_cs_n => DRAM_CS_N,
+		 sdram_wire_dq => DRAM_DQ,
+		 sdram_wire_dqm => DQM,
+		 sdram_wire_ras_n => DRAM_RAS_N,
+		 sdram_wire_we_n => DRAM_WE_N,
+		 
+		 lcd_data_DATA => LCD_DATA,
+		 lcd_data_ON => LCD_ON,
+		 lcd_data_BLON => LCD_BLON,
+		 lcd_data_EN => LCD_EN,
+		 lcd_data_RS => LCD_RS,
+		 lcd_data_RW => LCD_RW,
+		 
+		 sd_card_interface_b_SD_cmd => SD_CMD,
+		 sd_card_interface_b_SD_dat => SD_DAT,
+		 sd_card_interface_b_SD_dat3 => SD_DAT3,
+		 sd_card_interface_o_SD_clock => SD_CLK);
+end exercise_three_structure;
+	
+
+	
+	
+	
+	
+	
+	
---
+++ Exercise3/hello_world.c
@@ -0,0 +1,62 @@
+/*
+ * "Hello World" example.
+ *
+ * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
+ * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
+ * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
+ * device in your system's hardware.
+ * The memory footprint of this hosted application is ~69 kbytes by default
+ * using the standard reference design.
+ *
+ * For a reduced footprint version of this template, and an explanation of how
+ * to reduce the memory footprint for a given application, see the
+ * "small_hello_world" template.
+ *
+ */
+
+#include <stdio.h>
+#include "altera_up_sd_card_avalon_interface.h"
+
+int main()
+{
+	alt_up_sd_card_dev* sd_card_reference = NULL;
+	int connected = 0;
+	char* file_name;
+
+	//initialize sd card
+	sd_card_reference = alt_up_sd_card_open_dev("/dev/sd_card");
+
+	if (sd_card_reference != NULL)
+	{
+		// repeated check the connectivity of the SD CARD
+		while (alt_up_sd_card_is_Present() == false);
+		if (alt_up_sd_card_is_Present())
+		{
+			printf("Card connected.\n");
+			if (alt_up_sd_card_is_FAT16())
+			{
+				printf("FAT16 file system detected.\n");
+				if (alt_up_sd_card_find_first("directory", file_name) == 0)
+				{
+					printf("%s\n", file_name);
+					while (alt_up_sd_card_find_next(file_name) == 0)
+					{
+						printf("%s\n", file_name);
+					}
+					printf("Success: read completed.\n");
+				}
+				else
+				{
+					printf("Error: didn't read anything.\n");
+				}
+			}
+			else
+			{
+				printf("Unknown file system.\n");
+			}
+			printf("Terminating.\n");
+		}
+	}
+
+	return 0;
+}
