5673a49028c714be8d2d3e5f7fd965c04f0517f3
ht92 <ht92@live.ca>
2013-11-26 14:23:37 -0800

    update ai
    Basically extend raymond's idea ...
    
    AI have two modes of operation: Random and Directed...
    Random, it has no clue where to hit, so it randomly generate
    coordinates. When it hits something, then it inserts the 4 adjacent
    coordinates into a Stack, given if they have not been visited. If this
    Stack of potential targets is not empty, then the AI operates in
    Directed mode. Instead of generating random targets, it just pops from
    the stack.

--- Module 2/BattleShip/src/com/ece381/controller/ComputerPlayer.java
+++ Module 2/BattleShip/src/com/ece381/controller/ComputerPlayer.java
@@ -1,29 +1,29 @@
 package com.ece381.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Stack;
 
 import com.ece381.application.BattleShipApp;
 import com.ece381.models.Battleship;
 import com.ece381.models.GameBoard;
-import com.ece381.models.Tile;
+import com.ece381.models.ShipComponent;
+import com.ece381.models.WorldObject;
 import com.ece381.models.Battleship.ShipOrientation;
 
 public class ComputerPlayer {
 
 	private GameBoard aiGameBoard;
 	private int[][] hitMap; 
-	private int[] lastHitLocation;
-	private ArrayList<int[]> previousFireCoors;
-	private ArrayList<Integer> listOfDirTried;
+	private int[] lastHitLocation = null;
+	//keep track potential hit coordinates
+	private Stack<int[]> targets = new Stack<int[]>();
+	
+	//ai status
+	private int aiHP = 10;
 	
 	public ComputerPlayer()
 	{
 		generateGameBoard();
 		generateHitMap();
-		lastHitLocation = null;
-		previousFireCoors = new ArrayList<int[]>();
-		listOfDirTried = new ArrayList<Integer>();
 	}
 
 	private void generateGameBoard() {
@@ -67,89 +67,86 @@ public class ComputerPlayer {
 	public int[] computerFireCoordinates()
 	{
 		int[] fireCoor = new int[2];
-		//generate completely random coordinates when computer has no clue
-		if (lastHitLocation == null)
+		if (targets.isEmpty())
 		{
-			do {
-				fireCoor[0] = (int) (Math.random() * GameBoard.getBoardWidth());
-				fireCoor[1] = (int) (Math.random() * GameBoard.getBoardHeight());
-			}while (checkFireCoor(fireCoor));
-		}
-		
-		//if it previously hit a ship, try firing at the neighbouring locations
-		else {
-			int dir;		
-			
-			// if all 4 directions have been tried, fire at a random location 
-			if (listOfDirTried.size() == 4) {
+			//generate completely random coordinates when computer have no clue
+			if (lastHitLocation == null)
+			{
 				do {
 					fireCoor[0] = (int) (Math.random() * GameBoard.getBoardWidth());
 					fireCoor[1] = (int) (Math.random() * GameBoard.getBoardHeight());
-				} while (checkFireCoor(fireCoor));
-				
-				listOfDirTried.clear();
-				lastHitLocation = null;
+				}while (hitMap[fireCoor[0]][fireCoor[1]] == 1);
 			}
-			
-			else {
-				do {
-					//generate a random direction
-					do {
-						dir = (int) (Math.random() * 3);								
-					}
-					while (listOfDirTried.contains(dir));
-							
-					switch (dir) {
-						case 0: 
-							fireCoor[0] =  lastHitLocation[0];
-							fireCoor[1] =  lastHitLocation[1] + 1;
-							break;
-						case 1:
-							fireCoor[0] =  lastHitLocation[0];
-							fireCoor[1] =  lastHitLocation[1] - 1;
-							break;
-						case 2:
-							fireCoor[0] =  lastHitLocation[0] + 1;
-							fireCoor[1] =  lastHitLocation[1];
-							break;
-						case 3:
-							fireCoor[0] =  lastHitLocation[0] - 1;
-							fireCoor[1] =  lastHitLocation[1];
-							break;				
-					}	
-					listOfDirTried.add(dir); //keep track of the directions that have been tried
-					
-				} while (checkFireCoor(fireCoor) && listOfDirTried.size() < 4);		
-			}
-			
+		}
+		else 
+		{
+			fireCoor = targets.pop();
 		}
 		
-		
-		previousFireCoors.add(fireCoor); //keep track of the locations computer player has fired at
 		return fireCoor;
 	}
-
-
-	public GameBoard getAIGameBoard()
+	
+	public boolean receiveFireCommand(int[] fire_coordinates)
 	{
-		return aiGameBoard;
-	}
-
-	public void setHitLocation(int[] hitLocation) {
-		lastHitLocation = hitLocation.clone();
-		listOfDirTried.clear();
+		boolean gotHit = false;
+		WorldObject wo = aiGameBoard.getTileAt(
+				fire_coordinates[0], fire_coordinates[1]).getTileObject();
+		if (wo != null && wo instanceof ShipComponent)
+		{
+			//got hit, minus 1 hp
+			aiHP--;
+			gotHit = true;
+		}
+		return gotHit;
 	}
 	
-	public boolean checkFireCoor(int[] fireCoor) {
-		boolean isContained = false;
+	public void updateHitMap(int[] coordinates)
+	{
+		this.hitMap[coordinates[0]][coordinates[1]] = 1;
+	}
+	
+	public void updateTargetList(int[] coordinates)
+	{
+		int x = coordinates[0];
+		int y = coordinates[1];
 		
-		//check if it's one of the locations that has been fired at
-		for(int[] coor : previousFireCoors) {
-			if(Arrays.equals(fireCoor, coor))
-				isContained = true;
+		//north
+		if (y + 1 < GameBoard.getBoardHeight())
+		{
+			if (hitMap[x][y+1] == 0)
+			{
+				this.targets.push(new int[] {x, y+1});
+			}
 		}
-		
-		return (hitMap[fireCoor[0]][fireCoor[1]] == 1 || isContained);
+		//south
+		if (y - 1 >= 0)
+		{
+			if (hitMap[x][y-1] == 0)
+			{
+				this.targets.push(new int[] {x, y-1});
+			}
+		}
+		//east
+		if (x + 1 < GameBoard.getBoardWidth())
+		{
+			if (hitMap[x+1][y] == 0)
+			{
+				this.targets.push(new int[] {x+1, y});
+			}
+		}
+		//west
+		if (x - 1 >= 0)
+		{
+			if (hitMap[x-1][y] == 0)
+			{
+				this.targets.push(new int[] {x-1, y});
+			}
+		}
+	}
+	
+	public int getHP()
+	{
+		return this.aiHP;
 	}
 	
 }
